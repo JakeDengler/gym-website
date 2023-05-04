4a2caa90792708382092b866abee866a9633e0e3
JakeDengler <jakedengler25@gmail.com>
2023-05-04 14:02:02 -0400

    reworking sign up sheet

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const container = document.querySelector(".contactcontainer");
-const signUpBtn = document.querySelector(".logoBackground button");
+
+const container = document.querySelector(".contactContainer");
+const signUpBtn = document.querySelector(".greenBackground button");
 
 signUpBtn.addEventListener("click", () => {
   container.classList.toggle("change");
