99d7a8c5a1da8af4cb8d99576c1d66f7a6a3e57e
JakeDengler <jakedengler25@gmail.com>
2023-05-11 13:30:19 -0400

    fireship is teaching me how to do transitions:

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ const signUpBtn = document.querySelector(".greenBackground button");
 signUpBtn.addEventListener("click", () => {
   container.classList.toggle("change");
 });
+
+const observer = new IntersectionObserver((enttries)) => {
+  entries.forEach((entry)) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('boxsection');
+hiddenElements.forEach((el)) => observer.observe((el));
